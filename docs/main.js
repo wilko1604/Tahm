196aa021d6cb45a57ce3e6c0d7217f14abaf7fec
wilko1604 <wilko1604@users.noreply.github.com>
2020-11-11 23:17:52 +1100

    initial landing page work

---
+++ docs/main.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+var Lwindow = document.getElementById("Limg")
+
+function Lhover(element) {
+  element.setAttribute('src', 'images/window-left-T.jpg');
+}
+
+function Lunhover(element) {
+  element.setAttribute('src', 'images/window-left.jpg');
+}
+
+var Cwindow = document.getElementById("Cimg")
+
+function Chover(element) {
+  element.setAttribute('src', 'images/filecabinetV2.jpg');
+}
+
+function Cunhover(element) {
+  element.setAttribute('src', 'images/filecabinetV1.jpg');
+}
+
+var Rwindow = document.getElementById("Rimg")
+
+function Rhover(Rwindow) {
+  Rwindow.setAttribute('src', 'images/window-right-T.jpg');
+}
+
+function Runhover(Rwindow) {
+  Rwindow.setAttribute('src', 'images/window-right.jpg');
+}
